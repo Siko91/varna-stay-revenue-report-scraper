8d2c977d0010bbfb4d3822262d995ac7f0856934
Aleks <alexander.n.dinkov@gmail.com>
2024-10-28 19:56:28 +0200

    auto-update

--- _1_login.js
+++ _1_login.js
@@ -3,7 +3,7 @@ import { req } from "./_req.js";
 import { todayStr } from "./_today.js";
 
 var COOKIE =
-  "Path=/; Path=/; _ga=GA1.1.452480953.1725912848; intercom-device-id-dhsvljfu=93cdf9f8-b79c-45f0-980a-764ed00f8fae; app_identity=alexander.n.dinkov%40gmail.com; app_remember_code=PJBdmxVgGXL5TqlBeADVMu; hfy_session_app=nhshc71bq8iaqt6reqqasvvcbkrdmb2e; _ga_LYX8CKJ79W=GS1.1.1728900381.8.1.1728900381.60.0.621526466; intercom-session-dhsvljfu=eXZuaU1KUHhhaWRKajQwT2RvZmEzTFdxUWhoV3RIZGp0ZVYwWDNpTFh5cldkcFRTVzFEd2xJUDRNVWRIRVF0Yy0tcDI4aGJvRUpRcXZXNlVVMVlHRS9oZz09--77a01e425665810377a2a0e6ab5635b17fec9919";
+  "Path=/; Path=/; _ga=GA1.1.452480953.1725912848; intercom-device-id-dhsvljfu=93cdf9f8-b79c-45f0-980a-764ed00f8fae; app_identity=alexander.n.dinkov%40gmail.com; app_remember_code=PJBdmxVgGXL5TqlBeADVMu; hfy_session_app=cjl018o8of8md67tp51fnp1ehh739mhs; _ga_LYX8CKJ79W=GS1.1.1730138105.10.1.1730138113.52.0.1281659954; intercom-session-dhsvljfu=L3dLU200MHpRa0ZqdXJGQ3ZJeDY4aUEzaUFDSnpNT3NGT1QwM1RFbnI3RExGVGN1WVlXR0V2cE8veGRBWUhBRy0tSDUvM0xrRWZJODZJc2wrS21vQXpNUT09--de503f4f93484f0eb5c659e06123680d12b7a6de";
 
 run();
 export async function run() {
